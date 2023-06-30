ebda19efd04bb62627838b8024ff13869d7676a9
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2023-06-30 22:10:24 +0300

    переделал обработку исключений

---
+++ src/main/java/ru/skypro/homework/ExceptionApiHandler.java
@@ -0,0 +1,31 @@
+package ru.skypro.homework;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
+
+@RestControllerAdvice
+public class ExceptionApiHandler {
+    @ExceptionHandler(HttpClientErrorException.NotFound.class)
+    public ResponseEntity<String> notFoundException(HttpClientErrorException.NotFound exception) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(HttpClientErrorException.Unauthorized.class)
+    public ResponseEntity<String> unauthorizedException(HttpClientErrorException.Unauthorized exception) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(HttpClientErrorException.Forbidden.class)
+    public ResponseEntity<String> forbiddenException(HttpClientErrorException.Forbidden exception) {
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .body(exception.getMessage());
+    }
+}
