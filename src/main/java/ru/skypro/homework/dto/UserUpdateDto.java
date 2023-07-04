5d919e85532c3c419abd2728bb133daa7a566b54
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2023-07-04 22:24:33 +0300

    поля pk + @Id

--- src/main/java/ru/skypro/homework/dto/UserUpdateDto.java
+++ src/main/java/ru/skypro/homework/dto/UserUpdateDto.java
@@ -1,12 +1,10 @@
 package ru.skypro.homework.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
+@RequiredArgsConstructor
 public class UserUpdateDto {
     private String firstName;
     private String lastName;
